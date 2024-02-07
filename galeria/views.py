f6713fc14fe06e1953cee4f07ddf39ee6b93f759
nandonet8000 <nandonet407@gmail.com>
2024-02-06 23:34:21 -0300

    projeto curso parte 2_1

--- galeria/views.py
+++ galeria/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1 : {"nome" :  "Nebulosa de Carina",
+         "legenda" : "webbtelecope.org /NASA/ James Webb"
+         },
+
+    2 : {"nome" : "Galaxia NGC 1079",
+         "legenda" : "nasa.org /NASA / Hubble"}
+
+    }
+    return render(request, 'galeria/index.html', {"cards" : dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
