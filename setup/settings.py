547a9ff6202f80c29fcb79f72eb042ac3153292f
nandonet8000 <nandonet407@gmail.com>
2024-02-08 16:20:11 -0300

    projeto curso alura 7

--- setup/settings.py
+++ setup/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
-    'patrocinio'
+    'patrocinio.apps.PatrocinioConfig'
 ]
 
 MIDDLEWARE = [
