ccad6c2ecd4611b93464be11ce8f759397ee19a1
nandonet8000 <nandonet407@gmail.com>
2024-02-08 18:44:56 -0300

    projeto curso alura 5

--- galeria/models.py
+++ galeria/models.py
@@ -16,11 +16,11 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=False , blank=False)
     categoria = models.CharField(max_length=100,choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False , blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     data_fotografia = models.DateTimeField(default = datetime.now , blank = False)
     publicada = models.BooleanField(default=False)
     
 
 
     def __str__(self) -> str:
-        return f"Fotografia  Nome: {self.nome}"
+        return self.nome
