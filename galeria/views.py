58e5e6b86e95c6c44750c09437403573ff1afe2c
nandonet8000 <nandonet407@gmail.com>
2024-02-07 21:55:35 -0300

    projeto curso alura

--- galeria/views.py
+++ galeria/views.py
@@ -1,23 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from calcularData import test_automatizado
+from galeria.models import Fotografia
 # Create your views here.
 
 
 
 def index(request):
-    dados = {
-    1 : {"nome" :  "Nebulosa de Carina",
-         "legenda" : "webbtelecope.org /NASA/ James Webb",
-         "relogio" : test_automatizado()
-         },
-
-    2 : {"nome" : "Galaxia NGC 1079",
-         "legenda" : "nasa.org /NASA / Hubble",
-         "relogio" : test_automatizado()}
-
-    }
-    return render(request, 'galeria/index.html', {"cards" : dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards" : fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
